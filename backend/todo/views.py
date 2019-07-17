9013fe753c3c711dfea06cb1a55defadfc9a4c26
Mayan Annie Rothstein <mayan@bu.edu>
2019-07-17 11:34:24 -0700

    set up url paths

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+# viewsets provides implementation for CRUD operations by default, 
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import TodoSerializer
+from .models import Todo
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all() 
