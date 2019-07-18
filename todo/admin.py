dd9e0899cb1e86e969f22f87a86f3d96fd544c83
Mayan Annie Rothstein <mayan@bu.edu>
2019-07-18 15:12:24 -0700

    settings update

--- todo/admin.py
+++ todo/admin.py
@@ -5,4 +5,4 @@ class TodoAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'completed')
 
 # Register your models here.
-admin.site.register(Todo, TodoAdmin) # add this
+admin.site.register(Todo, TodoAdmin)
